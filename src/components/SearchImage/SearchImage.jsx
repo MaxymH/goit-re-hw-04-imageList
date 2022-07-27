7c71dcaeee158f181b2dffd4b9d5a6f58ff75c90
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-07-27 17:27:18 +0200

    test push

--- src/components/SearchImage/SearchImage.jsx
+++ src/components/SearchImage/SearchImage.jsx
@@ -7,7 +7,7 @@ import { Oval } from 'react-loader-spinner'
   import 'react-toastify/dist/ReactToastify.css';
 import { query } from "components/Api/pixabay";
 
-import { Component, useEffect, useState } from 'react'
+import {  useEffect, useState } from 'react'
 
 const SearchImage = () => {
 
@@ -25,7 +25,6 @@ const SearchImage = () => {
     })
     const [name, setName] = useState('')
     const [page, setPage] = useState(1)
-    const [totalHits, setTotalHits] = useState(null)
     
     useEffect(() => {
         
@@ -41,10 +40,10 @@ const SearchImage = () => {
             })
             try {
                 const { totalHits, hits } = await query(name, page);
-                setState(s => ({
-                    ...s,
+                setState(prevPage => ({
+                    ...prevPage,
                     loader: 'load',
-                    urlImage: [...s.urlImage, ...hits],
+                    urlImage: [...prevPage.urlImage, ...hits],
                     totalHits: totalHits,
                 }))
                 
@@ -59,8 +58,9 @@ const SearchImage = () => {
                 setState({ ...state, loader: 'load', error: error.message });
             }
         }
+        
             fetch()
-        },[name, page])
+        },[name, state])
 
     const onSubmit = (data) => {
         if (data === '') {
@@ -70,16 +70,13 @@ const SearchImage = () => {
             return
         }
         setName(data)
-        setTotalHits(null)
         setState({
-            urlImage: [], loader: false, error:"",
+            urlImage: [], loader: false, error:"",totalHits:null,
         }) 
     }
 
     const onClickLoadMore = () => {
-        setPage(s => ({
-            page: s + 1
-        }))
+        setPage(page + 1)
     }
 
     const onClickGalleryItem = (id) => {
@@ -136,111 +133,4 @@ const SearchImage = () => {
 }
 
 
-
- class SearchImages extends Component  {
-    state = {
-        modal: false,
-        name: '',
-        urlImage: [],
-        loader: false,
-        page: 1,
-        error: '',
-        totalHits: null,
-        modalUrl: [],
-    }
-    
-
-    // async componentDidUpdate(prevProp, prevState) {
-    // const { name, page} = this.state;
-    
-    //     if (name !== prevState.name || page > prevState.page) {
-    //         this.setState({ loader: 'loading' })
-    
-            
-      
-
-    //         try {
-    //             const { totalHits, hits } = await query(name, page);
-    //             this.setState(prevPage =>({
-    //                 totalHits: totalHits,
-    //                 urlImage: [...prevPage.urlImage, ...hits],
-    //             }))
-
-    //             if (totalHits === 0) {
-    //         toast.error("No result!", {
-    //             autoClose: 2000
-    //             })
-    //         }
-    //         } catch (error) {
-    //             this.setState({ error: error.message });
-    //         } finally {
-    //             this.setState({ loader: 'load' })     
-    //         }
-    //     }
-        
-    // }
-
-    // onSubmit = (data) => {
-    //     if (data === '') {
-    //         toast.info("Enter name!", {
-    //             autoClose: 2000
-    //         })
-    //         return
-    //     }
-    
-    //     this.setState({
-    //         name: data,
-    //         totalHits: null,
-    //         urlImage: [] ,
-    //     }) 
-    // }
-    
-    // onClickLoadMore = () => {
-    //     this.setState(prevPage => ({
-    //         page: prevPage.page + 1
-    //     }))
-        
-    // } 
-
-    // onClickGalleryItem = (id) => {
-    //     const { urlImage } = this.state
-    //     const image = urlImage.find(f => f.id === Number(id))
-    //     if (image) {
-    //         this.setState({
-    //             modalUrl: image,
-    //             modal: true,
-    //         })
-    //     }
-    // }
-
-    closeModal = () => {
-        this.setState({
-            modal: false,
-            modalUrl: [],
-        })
-    }
-
-    
-
-    render() {
-        const { onSubmit, onClickGalleryItem, onClickLoadMore, closeModal } = this
-        const {loader, urlImage, totalHits, modal } = this.state
-        return (
-            <>
-                <Searchbar onSubmit={onSubmit} />
-                <ImageGallery onClickGalleryItem={onClickGalleryItem} image={urlImage} />
-                {loader === 'loading' &&
-                    <Oval color="#00BFFF" height={80} width={80} />}
-                {totalHits > 10 && <Button func={onClickLoadMore} />}
-            
-                {modal &&
-                    <Modal closeModal={closeModal}>
-                    <img src={this.state.modalUrl.largeImageURL} alt={this.state.modalUrl.tags} />
-                </Modal>}
-                <ToastContainer />
-                
-        </>
-        )
-    }
-}
 export default SearchImage
